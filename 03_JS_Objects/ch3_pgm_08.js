e7c48fe34576930bd54b1e5425d66550e519540a
Nandha-Devi <2222ja31@kgcas,com>
2024-07-15 00:25:35 +0530

    initial commit

---
+++ 03_JS_Objects/ch3_pgm_08.js
@@ -0,0 +1,25 @@
+var book;
+
+book2 = {
+	title     : "The hobbit",
+	  author    : "j.r.r.tolkien",
+	  published : 1937
+  };
+
+console.log(book.title);
+console.log(book.author);
+
+console.log(book.published);
+
+
+var book2;
+
+book = {
+	title     : "The art of brave",
+	  author    : "Jenny lerew",
+	  published : 1937
+  };
+
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
